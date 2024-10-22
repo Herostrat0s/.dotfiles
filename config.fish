96157cfacb961bcff46d1af4ef3eb08bfa642f41
Herostrat0s <muhammed.arslan16@ogr.sakarya.edu.tr>
2024-10-22 17:46:31 +0300

    added second dotfile

---
+++ config.fish
@@ -0,0 +1,11 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/navi/anaconda3/bin/conda
+    eval /Users/navi/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+set -g fish_greeting
